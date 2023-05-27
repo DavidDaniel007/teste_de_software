8435bc9713f686cdc3cb02c08fad0b513b202c9b
David Daniel <102994274+DavidDaniel007@users.noreply.github.com>
2023-05-27 11:20:16 -0400

    Add files via upload

---
+++ conversaonumero.js
@@ -0,0 +1,5 @@
+//conversão de variáveis
+console.log(parseInt("2") + parseInt("2"))
+console.log("8" / "2")
+console.log("9" * "2")
+console.log("8" - "2")
---
+++ lista.js
@@ -0,0 +1,9 @@
+const listaDestino = new Array (
+  'Salvador',
+  'São Paulo',
+  'Rio de Janeiro',
+  'Manaus'
+);
+
+listaDestino.push ('Mexico');
+console.log(listaDestino)
